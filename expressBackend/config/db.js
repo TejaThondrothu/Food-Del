62676c62dfb8fa8dd2051c95c2eb1f20ca5995f9
TejaThondrothu <teja.thondrothu@napierhealthcare.com>
2024-06-08 17:19:55 +0530

    Added mongoDB connection key from .env file through config/db.js file and stored the images in server file

--- expressBackend/config/db.js
+++ expressBackend/config/db.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 export const connectDB = async () =>{
     // In this connect method , you need to add you Mongo DB connect link : In the below format It will be there
     //mongodb+srv://projectName:12561256@cluster0.jrlkptp.mongodb.net/food-del
-        await mongoose.connect("").then(
+        const secretKey = process.env.mongoDB_key;
+        await mongoose.connect(secretKey).then(
             ()=>console.log("DB Connected")
         );
 }
